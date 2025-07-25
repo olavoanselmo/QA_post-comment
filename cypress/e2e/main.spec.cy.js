e7e31873200721ac5d314af20df53b7a3e86f638
Olavo Anselmo <olavoanselmo@gmail.com>
2025-07-25 18:56:11 -0300

    First scenario cypress automation

--- cypress/e2e/main.spec.cy.js
+++ cypress/e2e/main.spec.cy.js
@@ -2,8 +2,11 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('localhost:5173/')
     cy.get('[data-qa="profile-box"]')
+    //abre o comentario e digita
     cy.get('[data-qa="comment-input"]').first().type('Test 123')
+    //publica o comentario clicando no botão
     cy.get('[data-qa="publish-button"]').first().click()
+    //verificar se contem a string Teste 123
     cy.get('[data-qa="comment-text"]').contains('Test 123')
     cy.get('[data-qa="comment-text"]').each(($element) => {
       if ($element.text() === 'Test 123') {
